2c79fcfce328b77cc75d6d22f8a4ac5565a40f72
Jaeyoung Choi <hideyf@gmail.com>
2020-01-25 19:00:01 +0900

    add default ACDColors

--- Sources/ACDColors/ACDColor+UIKit.swift
+++ Sources/ACDColors/ACDColor+UIKit.swift
@@ -12,9 +12,89 @@ import Foundation
 import UIKit
 
 extension ACDColor {
-    var uiColor: UIColor {
+    public var uiColor: UIColor {
         UIColor(color: color.uiColor, darkColor: darkColor?.uiColor, colorContrast: colorContrast?.uiColor, darkColorContrast: darkColorContrast?.uiColor)
     }
+    
+    public init(uiColor: UIColor) {
+        if #available(iOS 13.0, *) {
+            self.color = uiColor.lightColor.argbColor
+            self.darkColor = uiColor.darkColor.argbColor
+            self.colorContrast = uiColor.lightContrastColor.argbColor
+            self.darkColorContrast = uiColor.darkContrastColor.argbColor
+        } else {
+            self.color = uiColor.argbColor
+            self.darkColor = nil
+            self.colorContrast = nil
+            self.darkColorContrast = nil
+        }
+        
+    }
+}
+
+extension ACDColor {
+    public static let label: ACDColor = .init(uiColor: .label_)
+    public static let secondaryLabel: ACDColor = .init(uiColor: .secondaryLabel_)
+    public static let tertiaryLabel: ACDColor = .init(uiColor: .tertiaryLabel_)
+    public static let quaternaryLabel: ACDColor = .init(uiColor: .quaternaryLabel_)
+    
+    public static let systemFill: ACDColor = .init(uiColor: .systemFill_)
+    public static let secondarySystemFill: ACDColor = .init(uiColor: .secondarySystemFill_)
+    public static let tertiarySystemFill: ACDColor = .init(uiColor: .tertiarySystemFill_)
+    public static let quaternarySystemFill: ACDColor = .init(uiColor: .quaternarySystemFill_)
+        
+    public static let placeholderText: ACDColor = .init(uiColor: .placeholderText_)
+    
+    public static let systemBackground: ACDColor = .init(uiColor: .systemBackground_)
+    public static let secondarySystemBackground: ACDColor = .init(uiColor: .secondarySystemBackground_)
+    public static let tertiarySystemBackground: ACDColor = .init(uiColor: .tertiarySystemBackground_)
+    
+    public static let systemGroupedBackground: ACDColor = .init(uiColor: .systemGroupedBackground_)
+    public static let secondarySystemGroupedBackground: ACDColor = .init(uiColor: .secondarySystemGroupedBackground_)
+    public static let tertiarySystemGroupedBackground: ACDColor = .init(uiColor: .tertiarySystemGroupedBackground_)
+    
+    public static let separator: ACDColor = .init(uiColor: .separator_)
+    public static let opaqueSeparator: ACDColor = .init(uiColor: .opaqueSeparator_)
+    
+    public static let link: ACDColor = .init(uiColor: .link_)
+    
+    public static let darkText: ACDColor = .init(uiColor: .darkText)
+    public static let lightText: ACDColor = .init(uiColor: .lightText)
+    
+    public static let systemBlue: ACDColor = .init(uiColor: .systemBlue)
+    public static let systemGreen: ACDColor = .init(uiColor: .systemGreen)
+    public static let systemIndigo: ACDColor = .init(uiColor: .systemIndigo_)
+    public static let systemOrange: ACDColor = .init(uiColor: .systemOrange)
+    public static let systemPink: ACDColor = .init(uiColor: .systemPink)
+    public static let systemPurple: ACDColor = .init(uiColor: .systemPurple)
+    public static let systemRed: ACDColor = .init(uiColor: .systemRed)
+    public static let systemTeal: ACDColor = .init(uiColor: .systemTeal)
+    public static let systemYellow: ACDColor = .init(uiColor: .systemYellow)
+    
+    public static let systemGray: ACDColor = .init(uiColor: .systemGray)
+    public static let systemGray2: ACDColor = .init(uiColor: .systemGray2_)
+    public static let systemGray3: ACDColor = .init(uiColor: .systemGray3_)
+    public static let systemGray4: ACDColor = .init(uiColor: .systemGray4_)
+    public static let systemGray5: ACDColor = .init(uiColor: .systemGray5_)
+    public static let systemGray6: ACDColor = .init(uiColor: .systemGray6_)
+    
+    public static let clear: ACDColor = .init(uiColor: .clear)
+    
+    public static let black: ACDColor = .init(uiColor: .black)
+    public static let blue: ACDColor = .init(uiColor: .blue)
+    public static let brown: ACDColor = .init(uiColor: .brown)
+    public static let cyan: ACDColor = .init(uiColor: .cyan)
+    public static let darkGray: ACDColor = .init(uiColor: .darkGray)
+    public static let gray: ACDColor = .init(uiColor: .gray)
+    public static let green: ACDColor = .init(uiColor: .green)
+    public static let lightGray: ACDColor = .init(uiColor: .lightGray)
+    public static let magenta: ACDColor = .init(uiColor: .magenta)
+    public static let orange: ACDColor = .init(uiColor: .orange)
+    public static let purple: ACDColor = .init(uiColor: .purple)
+    public static let red: ACDColor = .init(uiColor: .red)
+    public static let white: ACDColor = .init(uiColor: .white)
+    public static let yellow: ACDColor = .init(uiColor: .yellow)
+    
 }
 
 #endif
--- Sources/ACDColors/UIColor+.swift
+++ Sources/ACDColors/UIColor+.swift
@@ -14,7 +14,7 @@ import UIKit
 
 extension UIColor {
     public convenience init(color: UIColor, darkColor: UIColor? = nil, colorContrast: UIColor? = nil, darkColorContrast: UIColor? = nil) {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             self.init(dynamicProvider: { (traitCollection) -> UIColor in
                 switch (traitCollection.userInterfaceStyle, traitCollection.accessibilityContrast) {
                 case (.dark, .high):
@@ -39,7 +39,7 @@ extension UIColor {
     }
 }
 
-@available(iOS 13.0, tvOS 13.0, *)
+@available(iOS 13.0, *)
 extension UIColor {
     public var lightColor: UIColor {
         resolvedColor(with: .init(userInterfaceStyle: .light))
@@ -47,6 +47,14 @@ extension UIColor {
     
     public var darkColor: UIColor {
         resolvedColor(with: .init(userInterfaceStyle: .dark))
+    }
+    
+    public var lightContrastColor: UIColor {
+        resolvedColor(with: .init(traitsFrom: [.init(userInterfaceStyle: .light), .init(accessibilityContrast: .high)]))
+    }
+    
+    public var darkContrastColor: UIColor {
+        resolvedColor(with: .init(traitsFrom: [.init(userInterfaceStyle: .dark), .init(accessibilityContrast: .high)]))
     }
 }
 
@@ -59,80 +67,76 @@ extension String {
 extension UIColor {
     // MARK: Label Colors
     public class var label_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .label
         } else {
             return ARGBColor.label.uiColor
         }
     }
     public class var secondaryLabel_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .secondaryLabel
         } else {
             return ARGBColor.secondaryLabel.uiColor
         }
     }
     public class var tertiaryLabel_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .tertiaryLabel
         } else {
             return ARGBColor.tertiaryLabel.uiColor
         }
     }
     public class var quaternaryLabel_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .quaternaryLabel
         } else {
             return ARGBColor.quaternaryLabel.uiColor
         }
     }
     
+    // MARK: Fill Colors
+    public class var systemFill_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .systemFill
+        } else {
+            return ARGBColor.systemFill.uiColor
+        }
+    }
+    
+    public class var secondarySystemFill_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .secondarySystemFill
+        } else {
+            return ARGBColor.secondarySystemFill.uiColor
+        }
+    }
+    
+    public class var tertiarySystemFill_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .tertiarySystemFill
+        } else {
+            return ARGBColor.tertiarySystemFill.uiColor
+        }
+    }
+    
+    public class var quaternarySystemFill_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .quaternarySystemFill
+        } else {
+            return ARGBColor.quaternarySystemFill.uiColor
+        }
+    }
+    
     // MARK: Text Colors
     public class var placeholderText_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .placeholderText
         } else {
             return ARGBColor.placeholderText.uiColor
         }
     }
     
-    // MARK: Separator Colors
-    public class var separator_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
-            return .separator
-        } else {
-            return ARGBColor.separator.uiColor
-        }
-    }
-    public class var opaqueSeparator_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
-            return .opaqueSeparator
-        } else {
-            return ARGBColor.opaqueSeparator.uiColor
-        }
-    }
-    
-    // MARK: Link Color
-    public class var link_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
-            return .link
-        } else {
-            return ARGBColor.link.uiColor
-        }
-    }
-    
-    // MARK: Adaptable Colors
-    public class var systemIndigo_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
-            return .systemIndigo
-        } else {
-            return ARGBColor.systemIndigo.uiColor
-        }
-    }
-}
-
-#if os(iOS)
-extension UIColor {
     // MARK: Standard Content Background Colors
     public class var systemBackground_: UIColor {
         if #available(iOS 13.0, *) {
@@ -142,14 +146,14 @@ extension UIColor {
         }
     }
     public class var secondarySystemBackground_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .secondarySystemBackground
         } else {
             return ARGBColor.secondarySystemBackground.uiColor
         }
     }
     public class var tertiarySystemBackground_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .tertiarySystemBackground
         } else {
             return ARGBColor.tertiarySystemBackground.uiColor
@@ -158,65 +162,98 @@ extension UIColor {
     
     // MARK: Grouped Content Background Colors
     public class var systemGroupedBackground_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .systemGroupedBackground
         } else {
             return ARGBColor.systemGroupedBackground.uiColor
         }
     }
     public class var secondarySystemGroupedBackground_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .secondarySystemGroupedBackground
         } else {
             return ARGBColor.secondarySystemGroupedBackground.uiColor
         }
     }
     public class var tertiarySystemGroupedBackground_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .tertiarySystemGroupedBackground
         } else {
             return ARGBColor.tertiarySystemGroupedBackground.uiColor
         }
     }
     
+    // MARK: Separator Colors
+    public class var separator_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .separator
+        } else {
+            return ARGBColor.separator.uiColor
+        }
+    }
+    public class var opaqueSeparator_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .opaqueSeparator
+        } else {
+            return ARGBColor.opaqueSeparator.uiColor
+        }
+    }
+    
+    // MARK: Link Color
+    public class var link_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .link
+        } else {
+            return ARGBColor.link.uiColor
+        }
+    }
+    
+    // MARK: Adaptable Colors
+    public class var systemIndigo_: UIColor {
+        if #available(iOS 13.0, *) {
+            return .systemIndigo
+        } else {
+            return ARGBColor.systemIndigo.uiColor
+        }
+    }
+    
     // MARK: Adaptable Gray Colors
     public class var systemGray2_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .systemGray2
         } else {
             return ARGBColor.systemGray2.uiColor
         }
     }
     public class var systemGray3_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .systemGray3
         } else {
             return ARGBColor.systemGray3.uiColor
         }
     }
     public class var systemGray4_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .systemGray4
         } else {
             return ARGBColor.systemGray4.uiColor
         }
     }
     public class var systemGray5_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .systemGray5
         } else {
             return ARGBColor.systemGray5.uiColor
         }
     }
     public class var systemGray6_: UIColor {
-        if #available(iOS 13.0, tvOS 13.0, *) {
+        if #available(iOS 13.0, *) {
             return .systemGray6
         } else {
             return ARGBColor.systemGray6.uiColor
         }
     }
 }
-#endif
 
 #endif
 
